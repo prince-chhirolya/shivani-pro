45401ec29b0d0283196483f5e7baf0b265939d18
Prince Chhirolya <prince.chhirolya04@gmail.com>
2025-06-14 18:57:25 +0530

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
@@ -95,6 +93,8 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      
     </div>
   );
 }
